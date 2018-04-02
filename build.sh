c27e8328d7cbfee0e9d19d7de7df6970a7de4171
mathias <mail@mathias.is>
2018-04-02 23:13:30 +0200

    Build changes

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 cd $HOME/build-websites/mathias-website/
+rm Gemfile.lock
+bundle install
 if jekyll build --destination $HOME/html/mathias.re/; then
 	cd $HOME/html/mathias.re/
 	rm -rf *.br
 	rm -rf *.gz
-	for file in `find . -type f -name '*' ! -name 'contact' ! -name 'report'`; do
+	for file in `find . -type f -name '*'`; do
 		$HOME/build-websites/zopfli $file
 		$HOME/build-websites/brotli $file
 	done
